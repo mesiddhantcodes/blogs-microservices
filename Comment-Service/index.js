86ed0986d779a121a9ee07c80ea9aae339428eb1
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-10-25 22:10:46 +0530

    [PATCH]

---
+++ Comment-Service/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+
+
+const commentByPostId = {}; 
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentByPostId[req.params.id]||[]);
+});
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentByPostId[req.params.id] || [];
+    comments.push({id:commentId,content});
+    commentByPostId[req.params.id]=comments;
+    res.status(201).send(comments);
+    // console.log(commentByPostId);
+   
+});
+
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
+});
